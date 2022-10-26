db85e5d7a6effc372bd7c256c798a7c1bbe3224c
Markson <marek4924@gmail.com>
2022-10-26 18:30:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TSKmgPROP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            menu menu = new menu();
+            int action = 0;
+
+            while (action != 4)
+            {
+
+                menu.drawMenu();
+
+                action = Convert.ToInt32(Console.ReadLine());
+
+                if (action == 1)
+                {
+                    Console.Clear();
+                    menu.drawTask();
+                    Console.WriteLine();
+                }
+
+                if (action == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Write a task to append: ");
+                    string task = Console.ReadLine();
+                    Console.Clear();
+                    menu.setTask(task);
+                }
+
+                if (action == 3)
+                {
+                    Console.Clear();
+                    int indexToDelete = Convert.ToInt32(Console.ReadLine());
+                    menu.setIndex(indexToDelete - 1);
+                    menu.delTask();
+                }
+
+                if (action == 4)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Closing...");
+                    Console.WriteLine();
+                }
+
+                if (action != 1 && action != 2 && action != 3 && action != 4)
+                {
+                    Console.Clear();
+                }
+            }
+        }
+    }
+
+}
---
+++ menu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSKmgPROP
+{
+    class menu
+    {
+        string filePath = @"YOUR'S FILE DIRECTIORY!";
+        int indexToDelete;
+
+        public void drawMenu()
+        {
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 - Show existing tasks");
+            Console.WriteLine("2 - Add new task");
+            Console.WriteLine("3 - Delete existing task");
+            Console.WriteLine("4 - Save'n exit");
+        }
+        public void setTask(string task)
+        {
+            File.AppendAllText(filePath, task + Environment.NewLine);
+        }
+
+        public void drawTask()
+        {
+            string[] insideFile = File.ReadAllLines(filePath);
+            int indexCounter = 0;
+            foreach (string inside in insideFile)
+            {
+                indexCounter++;
+                Console.WriteLine("[" + indexCounter + "] - " + inside);
+            }
+        }
+
+        public void setIndex(int indexToDelete)
+        {
+            this.indexToDelete = indexToDelete;
+        }
+
+        public void delTask()
+        {
+            var lineToDelete = new List<string>(File.ReadAllLines(filePath));
+            lineToDelete.RemoveAt(indexToDelete);
+            File.WriteAllLines(filePath, lineToDelete.ToArray());
+        }
+
+    }
+
+}
